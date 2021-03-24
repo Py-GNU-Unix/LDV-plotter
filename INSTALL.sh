9daf9cb5afde6cac2cc0e49583be328107104869
Luca Ravasio <luca.ravasio.1@gmail.com>
2021-03-24 22:59:23 +0100

    Changes to be committed: 	modified:   INSTALL.sh 	modified:   UNINSTALL.sh 	modified:   root/bin/ldv-plt 	modified:   root/usr/share/applications/LDV-plotter.desktop

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-python3 -m pip install venv
 python3 -m venv venv
 
 source venv/bin/activate
@@ -7,6 +6,7 @@ source venv/bin/activate
 python3 -m pip install -r REQUIREMENTS.txt
 
 mkdir /opt/LDV-plotter
-cp ./images ./scripts ./stylesheets ./venv /opt/LDV-plotter/
-cp -fr ./root /
+cp -r ./images ./scripts ./stylesheets ./venv /opt/LDV-plotter/
+cp ./root/bin/ldv-plt /bin/ldv-plt
+cp ./root/usr/share/applications/LDV-plotter.desktop /usr/share/applications/LDV-plotter.desktop
 
